ec0824cf9004233d9dc6f205b4427916c57c48bf
Nick Eisenberg <nickeisenberg@gmail.com>
2023-07-08 10:41:46 -0700

    using default shell

--- tmux_ide.sh
+++ tmux_ide.sh
@@ -1,20 +1,24 @@
 #!/usr/bin/env bash
 
+# after each tmux line you can specify the editor you want opened
+# eg: python, nvim, vim, bash, etc
+# Leaving it black will default to the standard shell
+
 function ide {
 	session="new"
 	tmux has-session -t $session
 	if [ $? != 0 ]
 		then
-			tmux new-session -s $session -n editor -d zsh
-			tmux split-window -t "$session:0.0" -h zsh
-			tmux split-window -t "$session:0.1" -v -p 5 zsh
+			tmux new-session -s $session -n editor -d  # <dif_editor_name>
+			tmux split-window -t "$session:0.0" -h 
+			tmux split-window -t "$session:0.1" -v -p 5 
 			tmux attach -t $session
 		else
 			tmux attach -t $session
 			tmux kill-session
-			tmux new-session -s $session -n editor -d zsh
-			tmux split-window -t "$session:0.0" -h zsh
-			tmux split-window -t "$session:0.1" -v -p 5 zsh
+			tmux new-session -s $session -n editor -d 
+			tmux split-window -t "$session:0.0" -h 
+			tmux split-window -t "$session:0.1" -v -p 5 
 			tmux attach -t $session
 			echo 'SESSION ALREADY EXISTS'
 		fi
