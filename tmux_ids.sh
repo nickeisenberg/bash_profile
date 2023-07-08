01c5e818b92689c20be5d15835d024c2495d305e
Nick Eisenberg <nickeisenberg@gmail.com>
2023-07-07 21:37:57 -0700

    tmux tile script

---
+++ tmux_ids.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+function ide {
+	session="new"
+	tmux has-session -t $session
+	if [ $? != 0 ]
+	then
+		tmux new-session -s $session -n editor -d zsh
+		tmux split-window -t "$session:0.0" -h zsh
+		tmux split-window -t "$session:0.1" -v -p 5 zsh
+		tmux attach -t $session
+	else
+		# tmux attach -t $session
+		# tmux kill-session
+		# tmux new-session -s $session -n editor -d zsh
+		# tmux split-window -t "$session:0.0" -h zsh
+		# tmux split-window -t "$session:0.1" -v -p 5 zsh
+		# tmux attach -t $session
+		echo 'SESSION ALREADY EXISTS'
+	fi
+}
