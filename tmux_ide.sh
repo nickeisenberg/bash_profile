171c7df0e7fd11915fb4f3a8fe24da37a036b258
Nick Eisenberg <nickeisenberg@gmail.com>
2023-07-08 07:39:18 -0700

    ide scipt will work if multiple terminals are open now

--- tmux_ide.sh
+++ tmux_ide.sh
@@ -10,12 +10,12 @@ function ide {
 		tmux split-window -t "$session:0.1" -v -p 5 zsh
 		tmux attach -t $session
 	else
-		# tmux attach -t $session
-		# tmux kill-session
-		# tmux new-session -s $session -n editor -d zsh
-		# tmux split-window -t "$session:0.0" -h zsh
-		# tmux split-window -t "$session:0.1" -v -p 5 zsh
-		# tmux attach -t $session
+		tmux attach -t $session
+		tmux kill-session
+		tmux new-session -s $session -n editor -d zsh
+		tmux split-window -t "$session:0.0" -h zsh
+		tmux split-window -t "$session:0.1" -v -p 5 zsh
+		tmux attach -t $session
 		echo 'SESSION ALREADY EXISTS'
 	fi
 }
